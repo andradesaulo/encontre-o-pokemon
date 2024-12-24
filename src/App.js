a3ff52768787b00abd1c83a3337876ca7cea2918
Saulo Andrade <andradesaulo.developer@gmail.com>
2024-12-24 15:51:40 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -168,6 +168,7 @@ const App = () => {
           isRisking={isRisking}
           gameOver={gameOver}
           handleReset={handleReset}
+          resetGame={resetGame}
         />
         <Announcer messages={announcerMessages} />
       </div>
